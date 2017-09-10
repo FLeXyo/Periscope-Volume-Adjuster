f6bea6427dfb790f65925806d93eeae4ba8a8c4d
FLeX <ahjflex@gmail.com>
2017-09-10 20:49:47 +0200

    Updated for new periscope and removed hearts

--- Periscope Volume Adjuster.user.js
+++ Periscope Volume Adjuster.user.js
@@ -1,37 +1,43 @@
 // ==UserScript==
 // @name         Periscope Volume Adjuster
 // @namespace    http://github.com/FLeXyo
-// @version      0.1
+// @version      0.2
 // @description  Allows you to adjust the volume of Periscope streams
 // @author       FLeX
-// @match        https://www.periscope.tv/*
+// @include        *://www.periscope.tv/*
+// @include        *://www.pscp.tv/*
 // @grant        none
 // @run-at document-idle
 // ==/UserScript==
 (function()
- {
-    'use strict';
-    var buttonDiv;
-    var checkPageLoadInterval = setInterval(function()
-                                            { buttonDiv = document.getElementsByClassName("GetAppCTA")[0]; var volumeButtonContainer = document.createElement("div");
-                                             volumeButtonContainer.className = "AppLink VolumeButtonContainer";
-                                             volumeButtonContainer.innerHTML = "<span>Volume</span><br/>";
-                                             for (var i = 1; i < 11; i++)
-                                             {
-                                                 (function(i){
-                                                     var volButton = document.createElement('button');
-                                                     volButton.id = "volButton" + i;
-                                                     volButton.addEventListener("click", function(){document.getElementById('PeriscopeVideoqb').volume = (i * 0.1);}, false);
-                                                     volButton.innerText = Math.round( (i * 0.1) * 10 ) / 10;
-                                                     volumeButtonContainer.appendChild(volButton);
-                                                 }(i));
+{
+  'use strict';
+  var overlayBarLeft;
+  var volumeSliderContainer = document.createElement("div");
+  volumeSliderContainer.class = "VolumeSliderContainer";
+  var slider = document.createElement("input");
+  slider.id = "volumeSlider";
+  slider.type = "range";
+  slider.min = 0;
+  slider.max = 1;
+  slider.value = 1;
+  slider.class = "slider";
+  slider.step = 0.1;
+  slider.oninput = function()
+  {
+    for (var i = 0; i < document.getElementsByTagName("video").length; i++) { document.getElementsByTagName("video")[i].volume = slider.value; }
+  }
+  volumeSliderContainer.appendChild(slider);
 
-                                             }
-                                             buttonDiv.appendChild(volumeButtonContainer);
-                                             if (buttonDiv !== undefined)
-                                             {
-                                                 clearInterval(checkPageLoadInterval);
-                                             }
-                                            }, 1000);
+  var checkPageLoadInterval = setInterval(function()
+  {
+    overlayBarLeft = document.getElementsByClassName("VideoOverlayRedesign-BottomBar-Left")[0];
+    overlayBarLeft.appendChild(volumeSliderContainer);
+    if (overlayBarLeft !== undefined)
+    {
+      document.getElementsByClassName("HeartsContainer")[0].outerHTML=""; //Remove hearts
+      clearInterval(checkPageLoadInterval);
+    }
+  }, 1000);
 
 })();
